eb9928b605d0f85321a1c1f975d10dee3844b0e7
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-07-09 10:19:36 +0900

    [Add]4-3

---
+++ chap04/oddOrEven.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var num = Math.round(Math.random() * 100);
+console.log("num\u306E\u5024: ".concat(num));
+var rem = num % 2;
+if (rem == 0) {
+    console.log("偶数");
+}
+else {
+    console.log("奇数");
+}
+console.log("処理終了");
