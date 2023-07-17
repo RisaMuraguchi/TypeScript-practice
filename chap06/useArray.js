3f20833e8ab9680d4f6f98561f5f58750a928bc6
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-07-17 18:43:02 +0900

    [Add]6-1

---
+++ chap06/useArray.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var list = [3, 7, 11, 16, 19];
+console.log("1\u500B\u76EE: ".concat(list[0]));
+console.log("2\u500B\u76EE: ".concat(list[1]));
+console.log("3\u500B\u76EE: ".concat(list[2]));
+console.log("4\u500B\u76EE: ".concat(list[3]));
+console.log("5\u500B\u76EE: ".concat(list[4]));
---
+++ chap06/useArray.ts
@@ -0,0 +1,8 @@
+export{}
+
+const list: number[] = [3, 7, 11, 16, 19];
+console.log(`1個目: ${list[0]}`);
+console.log(`2個目: ${list[1]}`);
+console.log(`3個目: ${list[2]}`);
+console.log(`4個目: ${list[3]}`);
+console.log(`5個目: ${list[4]}`);
