0c0dbdd353623bd794f5eddf0c3567ba060acbb4
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-07-17 18:47:40 +0900

    [Add]6-1

---
+++ chap06/useArrayAndFor.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var list = [3, 7, 11, 16, 19];
+for (var i = 0; i <= 4; i++) {
+    console.log("".concat(i, "\u500B\u76EE: ").concat(list[i]));
+}
---
+++ chap06/useArrayAndFor.ts
@@ -0,0 +1,7 @@
+export{}
+
+const list: number[] = [3, 7, 11, 16, 19];
+
+for(let i = 0; i <= 4; i++) {
+  console.log(`${i}個目: ${list[i]}`);
+}
