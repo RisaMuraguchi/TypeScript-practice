b28104b1135243c6170f700e7a213d95bc83d2ae
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-07-28 23:23:53 +0900

    [Add]3-2

---
+++ chap03/operation1.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var num1 = 500;
+var num2 = 50;
+var ans = num1 / num2;
+console.log("\u8A08\u7B97\u7D50\u679Cans\u306F".concat(ans));
---
+++ chap03/operation1.ts
@@ -0,0 +1,6 @@
+export{}
+
+const num1 = 500;
+const num2 = 50;
+const ans = num1 / num2;
+console.log(`計算結果ansは${ans}`);
