def3741be9dd28b7df3c5380d24f74add2314f96
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-07-29 21:04:11 +0900

    [Finish]練習問題3

---
+++ chap03/operation3.ts
@@ -0,0 +1,11 @@
+export{}
+
+let num1 = 10;
+let num2 = 3;
+let ans = num1 % num2;
+console.log(`num1は${num1}`);
+console.log(`num2は${num2}`);
+console.log(`num1÷num2のあまりansは${ans}`);
+ans ++;
+console.log(`さらに1足すと${ans}`);
+
