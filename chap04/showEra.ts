ceed024086bfcf4fb612d74f29439c8285bc23c8
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-08-01 19:04:40 +0900

    [Add]chap04練習問題

---
+++ chap04/showEra.ts
@@ -0,0 +1,15 @@
+export{}
+
+const year = Math.round(Math.random() * 120) + 1901;
+
+if(year < 1912) {
+  console.log("明治");
+} else if(year < 1926) {
+  console.log("大正");
+} else if(year < 1989) {
+  console.log("昭和");
+} else if(year < 2019) {
+  console.log("平成");
+} else {
+  console.log("令和");
+}
