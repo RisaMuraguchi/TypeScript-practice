5f7e5c744eef852aeeebec324b4ca66069abc31a
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-08-03 18:53:51 +0900

    [Add]chap05練習問題

---
+++ chap05/squareWhile.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var i = 1;
+while (i <= 10) {
+    var ans = Math.pow(i, 2);
+    console.log("".concat(i, "\u56DE\u76EE\u306E\u7D50\u679C: ").concat(ans));
+    i++;
+}
