75451717ca5fcabb5b09e07af02e2f430710c9e1
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-08-03 18:57:51 +0900

    [Add]chap05練習問題

---
+++ chap05/squareFor.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+for (var i = 1; i <= 10; i++) {
+    var ans = Math.pow(i, 2);
+    console.log("".concat(i, "\u56DE\u76EE\u306E\u7D50\u679C: ").concat(ans));
+}
