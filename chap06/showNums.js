5b93581bff70a66ba6db98414fc7a0e5f4114a05
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-08-07 13:28:45 +0900

    [Add]chap06練習問題

---
+++ chap06/showNums.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var list = [15, 36, 21, 48, 64, 59, 7];
+for (var i = 0; i <= 6; i++) {
+    console.log("".concat(i + 1, "\u500B\u76EE: ").concat(list[i]));
+}
---
+++ chap06/showNums.ts
@@ -0,0 +1,7 @@
+export{}
+
+const list: number[] = [15, 36, 21, 48, 64, 59, 7];
+
+for(let i = 0; i <= 6; i++) {
+  console.log(`${i + 1}個目: ${list[i]}`);
+}
