db6dc3ee0540c6be2ef438900b32c694e7fdffff
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-08-09 18:54:08 +0900

    [Finish]chap06練習問題

---
+++ chap06/showStudentMap.js
@@ -0,0 +1,10 @@
+// export{}
+const showStudent = new Map();
+showStudent.set("い組", 35);
+showStudent.set("ろ組", 36);
+showStudent.set("は組", 37);
+showStudent.set("に組", 34);
+showStudent.set("ほ組", 36);
+for (const [key, value] of showStudent) {
+    console.log(`${key}: ${value}人`);
+}
