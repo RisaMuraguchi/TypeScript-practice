869c0eb7a90dfe264ace84a8d482f81cff30d0d1
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-08-19 18:32:31 +0900

    [Add]9-3

---
+++ chap09/useConstructor.ts
@@ -0,0 +1,31 @@
+export{}
+
+
+class Student {
+  name: string = "";
+  english: number = 0;
+  math: number = 0;
+  japanese: number = 0;
+
+  constructor(name: string, english: number, math: number, japanese: number) {
+    this.name = name;
+    this.english = english;
+    this.math = math;
+    this.japanese = japanese;
+  }
+
+  showScoresSum() {
+    const sum = this.english + this.math + this.japanese;
+    console.log(`${this.name}の合計点: ${sum}`);
+  }
+
+  showScoreAvg() {
+    const sum = this.english + this.math + this.japanese;
+    const avg = Math.round(sum / 3 * 10) / 10;
+    console.log(`${this.name}の平均点: ${avg}`);
+  }
+}
+
+const taro = new Student("宮本太朗", 78, 82, 85);
+taro.showScoresSum();
+taro.showScoreAvg();
