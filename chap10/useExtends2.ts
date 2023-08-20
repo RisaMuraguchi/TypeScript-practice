39a0c2b9a592d218265069b807375b6dacbee49e
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-08-21 08:37:40 +0900

    [Add]10-1

---
+++ chap10/useExtends2.ts
@@ -0,0 +1,26 @@
+export{}
+
+class Greetings {
+  // 名前のプロパティ
+  protected name: string = "";
+
+  // コンストラクタ　名前を受け取りプロパティに格納する
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  // 「こんにちは」と表示するメソッド
+  sayHello() {
+    console.log(`${this.name}さん、こんにちは。`)
+  }
+}
+
+class GoodMorning extends Greetings {
+  sayGoodMorning() {
+    console.log(`${this.name}さん、おはようございます`);
+  }
+}
+
+const taro = new GoodMorning("江口太朗");
+taro.sayGoodMorning();
+taro.sayHello();
