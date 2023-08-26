e6144b724fa25c440a7df3a26b085dee87a1ab8e
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-08-26 21:51:25 +0900

    [Add]11-3

---
+++ chap11/module4/useCircles2.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var circles = require("../module2/Circles");
+var radius = 5;
+var ans = circles.calcAreaOfCircle(radius, circles.PI);
+console.log("\u8A08\u7B97\u3055\u308C\u305F\u7D50\u679C".concat(ans));
