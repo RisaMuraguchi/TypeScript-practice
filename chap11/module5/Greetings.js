3826504d96c37aa0024e4f84236ff42a93ef83b1
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-08-26 21:58:39 +0900

    [Finish]11-3

---
+++ chap11/module5/Greetings.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Greetings = /** @class */ (function () {
+    function Greetings(name) {
+        this.name = "";
+        this.name = name;
+    }
+    Greetings.prototype.sayHello = function () {
+        console.log("".concat(this.name, "\u3055\u3093\u3001\u3053\u3093\u306B\u3061\u306F\u3002"));
+    };
+    return Greetings;
+}());
+exports.default = Greetings;
---
+++ chap11/module5/useModule.ts
@@ -0,0 +1,4 @@
+import Greet from "./Greetings";
+
+const taro = new Greet("江口太朗");
+taro.sayHello();
