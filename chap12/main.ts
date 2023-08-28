bab8fdccaa642a3eba4a080729db3df5efa69cfe
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-08-29 07:14:55 +0900

    [Add]12-2

---
+++ chap12/main.ts
@@ -0,0 +1,20 @@
+// receiveWeatherInfo関数をインポート
+import {receiveWeatherInfo} from "./weatherinfo-receiver";
+
+// アクセス先URLの基本部分の変数を用意
+const weatherinfoUrl = "http://api.openweathermap.org/data/2.5/weather";
+// クエリパラメータの元データとなるオブジェクトリテラルを用意
+const params:{
+  lang: string,
+  q: string,
+  appId: string
+} =
+{
+  // 言語設定のクエリパラメータ
+  lang: "ja",
+  // 都市名を表すクエリパラメータ
+  q: "Himeji",
+  // APIキーのクエリパラメータ
+  appId: 
+}
+// クエリパラメータを生成
